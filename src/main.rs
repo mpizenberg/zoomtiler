298b8cb8e654ca1c31fc40f3db26c68f2b69b990
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-10-20 15:01:52 +0200

    Compute half resolutions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use anyhow::Context;
 use image::io::Reader as ImageReader;
-use image::{GenericImage, GenericImageView, RgbImage};
+use image::{GenericImage, GenericImageView, Rgb, RgbImage};
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
@@ -54,23 +54,129 @@ fn run(c: &seahorse::Context) -> anyhow::Result<()> {
     eprintln!("width_levels: {}", width_levels);
     eprintln!("levels: {}", levels);
 
-    // Start generating the images at the highest resolution level.
-    // TODO: when vertical panoramas inputs will be allowed,
-    // be careful with the image access order.
-    let mut extractor = ImgExtractor::new(&img_paths, &img_sizes);
-    for tx in 0..tile_count_width {
-        for ty in 0..tile_count_height {
-            let img = extractor.extract(tile_size, tx, ty)?;
-            let img_path = format!("tiles/{}_{}_{}.jpg", levels - 1, tx, ty);
-            img.save(&img_path)?;
-        }
-    }
+    // // Start generating the images at the highest resolution level.
+    // // TODO: when vertical panoramas inputs will be allowed,
+    // // be careful with the image access order.
+    // let mut extractor = ImgExtractor::new(&img_paths, &img_sizes);
+    // for tx in 0..tile_count_width {
+    //     for ty in 0..tile_count_height {
+    //         let img = extractor.extract(tile_size, tx, ty)?;
+    //         let img_path = format!("tiles/{}_{}_{}.jpg", levels - 1, tx, ty);
+    //         img.save(&img_path)?;
+    //     }
+    // }
 
     // Now, we need to take 2x2 blocs of images
     // and complete the pyramid of levels by halfing the resolution each time.
-    todo!();
+    let mut parent_x_tiles = tile_count_width;
+    let mut parent_y_tiles = tile_count_height;
+    for parent_level in (0..levels).rev() {
+        let (child_x_tiles, child_y_tiles) =
+            compute_half_resolutions(parent_level, parent_x_tiles, parent_y_tiles)?;
+        parent_x_tiles = child_x_tiles;
+        parent_y_tiles = child_y_tiles;
+    }
 
     Ok(())
+}
+
+/// Compute half resolution images and output the number of tiles generated.
+fn compute_half_resolutions(
+    previous_lvl: usize,
+    tile_count_width: usize,
+    tile_count_height: usize,
+) -> anyhow::Result<(usize, usize)> {
+    let half_tile_count_width = (tile_count_width + 1) / 2;
+    let half_tile_count_height = (tile_count_height + 1) / 2;
+    for tx in 0..half_tile_count_width {
+        for ty in 0..half_tile_count_height {
+            let img_path = |x, y| format!("tiles/{}_{}_{}.jpg", previous_lvl, x, y);
+            let top_left: RgbImage = ImageReader::open(img_path(tx * 2, ty * 2))?
+                .decode()?
+                .into_rgb8();
+            let top_right: RgbImage = match ImageReader::open(img_path(tx * 2 + 1, ty * 2)) {
+                Ok(reader) => reader.decode()?.into_rgb8(),
+                Err(_) => RgbImage::new(0, top_left.height()),
+            };
+            let bottom_left: RgbImage = match ImageReader::open(img_path(tx * 2, ty * 2 + 1)) {
+                Ok(reader) => reader.decode()?.into_rgb8(),
+                Err(_) => RgbImage::new(top_left.width(), 0),
+            };
+            let bottom_right: RgbImage = match ImageReader::open(img_path(tx * 2 + 1, ty * 2 + 1)) {
+                Ok(reader) => reader.decode()?.into_rgb8(),
+                Err(_) => RgbImage::new(top_right.width(), bottom_left.height()),
+            };
+            let half_img: RgbImage = half_res(top_left, top_right, bottom_left, bottom_right);
+            half_img.save(format!("tiles/{}_{}_{}.jpg", previous_lvl - 1, tx, ty))?;
+        }
+    }
+    Ok((half_tile_count_width, half_tile_count_height))
+}
+
+fn half_res(
+    top_left: RgbImage,
+    top_right: RgbImage,
+    bottom_left: RgbImage,
+    bottom_right: RgbImage,
+) -> RgbImage {
+    assert_eq!(top_left.width(), bottom_left.width());
+    assert_eq!(top_left.height(), top_right.height());
+    assert_eq!(bottom_right.width(), top_right.width());
+    assert_eq!(bottom_right.height(), bottom_left.height());
+    let half_width = (top_left.width() + top_right.width() + 1) / 2;
+    let half_height = (top_left.height() + bottom_left.height() + 1) / 2;
+
+    // Helper closure to use the correct image to retrieve a given pixel.
+    let img_source_and_offset = |x, y| {
+        if x < top_left.width() && y < top_left.height() {
+            (&top_left, 0, 0)
+        } else if x < top_left.width() {
+            (&bottom_left, 0, top_left.height())
+        } else if y < top_left.height() {
+            (&top_right, top_left.width(), 0)
+        } else {
+            (&bottom_right, top_left.width(), top_left.height())
+        }
+    };
+    // Helper closure to extract the correct pixel from the correct image.
+    let extract_pixel = |target_x, target_y| {
+        let (img, offset_x, offset_y) = img_source_and_offset(target_x, target_y);
+        let local_x = target_x - offset_x;
+        let local_y = target_y - offset_y;
+        if local_x >= img.width() || local_y >= img.height() {
+            None
+        } else {
+            Some(*img.get_pixel(local_x, local_y))
+        }
+    };
+    RgbImage::from_fn(half_width, half_height, |x, y| {
+        let pixels = [
+            extract_pixel(2 * x, 2 * y),
+            extract_pixel(2 * x + 1, 2 * y),
+            extract_pixel(2 * x, 2 * y + 1),
+            extract_pixel(2 * x + 1, 2 * y + 1),
+        ];
+        // Compute the mean of the 4 subpixels (maybe less if near a border)
+        let (pix_count, pix_sum): (u16, [u16; 3]) = IntoIterator::into_iter(pixels).fold(
+            (0, [0, 0, 0]),
+            |(count, [r_sum, g_sum, b_sum]), maybe_pix| match maybe_pix {
+                Some(pix) => (
+                    count + 1,
+                    [
+                        r_sum + pix.0[0] as u16,
+                        g_sum + pix.0[1] as u16,
+                        b_sum + pix.0[2] as u16,
+                    ],
+                ),
+                None => (count, [r_sum, g_sum, b_sum]),
+            },
+        );
+        Rgb([
+            (pix_sum[0] / pix_count) as u8,
+            (pix_sum[1] / pix_count) as u8,
+            (pix_sum[2] / pix_count) as u8,
+        ])
+    })
 }
 
 struct ImgExtractor {
